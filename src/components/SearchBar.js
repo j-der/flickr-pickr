f46eda30935965308487eb769ff539d819a9d4a7
Jasmine Der <jasmineder@gmail.com>
2016-04-05 16:34:50 -0400

    json get request working, TODO: display in component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,10 +10,23 @@ export default class SearchBar extends Component {
   searchSubmit = (event) => {
     let apiKey = "e301b1792cb18509d3b3c843227991d2"
     let searchTerm = this.refs.keyword.value
+    let numResults = 10
+    let numPages = 1
 
-    let flickrUrl = "https://api.flickr.com/services/rest/?api_key=${apiKey}&method=flickr.photos.search&format=json&nojsoncallback=1&&per_page=10&page=1&text=${searchTerm}"
+    let flickrUrl = `https://api.flickr.com/services/rest/?api_key=${apiKey}&method=flickr.photos.search&format=json&nojsoncallback=1&&per_page=${numResults}&page=${numPages}&text=${searchTerm}`
 
-    console.log("button clicked, yo")
+    $.getJSON(flickrUrl, {
+      api_key: apiKey,
+      tags: searchTerm,
+      per_page: numResults,
+      page: numPages,
+      format: "json"
+    }).success((data)=> {
+      console.log(data)}).error((err) => {
+        console.log(err)
+      })
+
+
     event.preventDefault()
 
   }
